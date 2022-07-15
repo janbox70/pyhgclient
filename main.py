519b2295c35c0dc47efefae6d2fb5ce2dd1939f9
janbox70 <88643344+janbox70@users.noreply.github.com>
2022-07-15 14:19:08 +0800

    support kout-post

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-
 import pandas as pd
 from src import HugeGraphRestClient
 
 # url_base = "http://10.14.139.71:8085/graphspaces/DEFAULT/graphs/hugegraph/"
-url_base = "http://10.14.139.69:8080/graphspaces/DEFAULT/graphs/hugegraph/"
+url_base = "http://10.14.139.69:8085/graphspaces/DEFAULT/graphs/hugegraphn/"
 auth = ("admin", "admin")
 
 
-def test_twitter_kout():
+def test_twitter_kout_get():
     client = HugeGraphRestClient(url_base, auth)
 
     vids = [20727483, 50329304, 26199460, 1177521, 27960125,
@@ -36,7 +35,7 @@ def test_twitter_kout():
                 result[vid].update({
                     'cost-{}'.format(depth): m['cost'],
                     'count-{}'.format(depth): len(r['vertices']),
-                    'iters/s-{}'.format(depth): int(1000*(m['edge_iters'] + m['vertice_iters'])/m['cost'])})
+                    'iters/s-{}'.format(depth): int(1000 * (m['edge_iters'] + m['vertice_iters']) / m['cost'])})
             else:
                 print(r)
 
@@ -45,7 +44,51 @@ def test_twitter_kout():
     print(df)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    test_twitter_kout()
+def test_twitter_kout_post():
+    client = HugeGraphRestClient(url_base, auth)
 
+    vids = [20727483, 50329304, 26199460, 1177521, 27960125,
+            30440025, 15833920, 15015183, 33153097, 21250581]
+
+    # 接口只支持最多 2000万
+    max_cap = 20000000
+    result = {}
+    for depth in range(1, 3):
+        for vid in vids:
+            r = client.do_kout_post({
+                "source": vid,
+                "steps": {
+                    "direction": "BOTH",
+                    "max_degree": max_cap,
+                    "skip_degree": max_cap
+                },
+                "max_depth": depth,
+                "nearest": True,
+                "limit": max_cap,
+                "with_vertex": False,
+                "with_path": False,
+                "count_only": True
+            })
+
+            if vid not in result:
+                result[vid] = {}
+
+            if "measure" in r:
+                # print(r)
+                m = r['measure']
+                print("vid: {}  count: {}  {}".format(vid, r['size'], m))
+                result[vid].update({
+                    'cost-{}'.format(depth): m['cost'],
+                    'count-{}'.format(depth): r['size'],
+                    'iters/s-{}'.format(depth): int(1000 * (m['edge_iters'] + m['vertice_iters']) / m['cost'])})
+            else:
+                print(r)
+
+    df = pd.DataFrame(result).T
+    df.loc['avg'] = df.mean().astype(dtype="int32")
+    print(df)
+
+
+if __name__ == '__main__':
+    test_twitter_kout_get()
+    # test_twitter_kout_post()
--- src/hgrestclient.py
+++ src/hgrestclient.py
@@ -8,12 +8,25 @@ class HugeGraphRestClient:
         self.auth = auth
 
     def do_get(self, api, params):
-        r = requests.get(self.url_base + api, params, auth=self.auth)
-        if r.status_code != 200:
-            raise "requests get failed: {}".format(r.status_code)
-        return r.json()
+        url = self.url_base + api
+        r = requests.get(url, params, auth=self.auth)
+        if r.status_code == 200:
+            return r.json()
+        print("get({}) failed({}): {}".format(url, r.status_code, r.content))
+        return r
+
+    def do_post(self, api, params):
+        url = self.url_base + api
+        r = requests.post(url, json=params, auth=self.auth)
+        if r.status_code == 200:
+            return r.json()
+        print("post({}) failed({}): {}".format(url, r.status_code, r.content))
+        return r
 
     def do_kout_get(self, params):
         r = self.do_get("traversers/kout", params)
         return r
 
+    def do_kout_post(self, params):
+        r = self.do_post("traversers/kout", params)
+        return r
